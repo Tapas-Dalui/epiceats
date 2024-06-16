7dbdea520b58fd31ad7e5d0709dcdd87ba360f30
Tapas <tdalui865@gmail.com>
2024-06-16 12:39:18 +0530

    changes in increment and decrement cart button

--- src/components/FoodItem/FoodItem.jsx
+++ src/components/FoodItem/FoodItem.jsx
@@ -1,13 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './FoodItem.css'
 import { assets } from '../../assets/assets'
 
 function FoodItem({ id, name, price, description, image }) {
+    
+    const [itemCount, setItemCount] = useState(0)
+    
     return (
         <>
             <div className="food-item">
                 <div className="food-item-image-container">
                     <img className='food-item-image' src={image} alt="" />
+                    {
+                        
+                            <div className='food-item-counter' >
+                                <img src={assets.remove_icon_red} onClick={ () => setItemCount( prev => prev-1 < 0 ? 0 : prev-1) } />
+                                <p>{itemCount}</p>
+                                <img src={assets.add_icon_green} onClick={ () => setItemCount(prev => prev+1 > 5 ? 5 : prev+1) } />
+                            </div>
+                    }
                 </div>
                 <div className="food-item-info">
                     <div className="food-item-name-rating">
