eb360438c036d77f4e50d380b291e44ddd9c0eec
Tapas <tdalui865@gmail.com>
2024-06-16 14:35:50 +0530

    Adding Filter in Food Section

--- src/components/FoodDisplay/FoodDisplay.jsx
+++ src/components/FoodDisplay/FoodDisplay.jsx
@@ -3,19 +3,21 @@ import './FoodDisplay.css'
 import { StoreContext } from '../../Context/StoreContext'
 import FoodItem from '../FoodItem/FoodItem'
 
-function FoodDisplay({category}) {
+function FoodDisplay({ category }) {
 
     const { food_list } = useContext(StoreContext)
     return (
         <>
-        <div className="food-display" id='food-display'>
-            <h2>Top Dishes Near You</h2>
-            <div className="food-diasplay-list">
-                { food_list.map( (item, index)=>{
-                    return <FoodItem key={index} id={item._id} name={item.name} description={item.description} price={item.price} image={item.image}   />
-                } ) }
+            <div className="food-display" id='food-display'>
+                <h2>Top Dishes Near You</h2>
+                <div className="food-diasplay-list">
+                    {food_list.map((item, index) => {
+                        if (category === 'All' || category === item.category) {
+                            return <FoodItem key={index} id={item._id} name={item.name} description={item.description} price={item.price} image={item.image} />
+                        }
+                    })}
+                </div>
             </div>
-        </div>
         </>
     )
 }
