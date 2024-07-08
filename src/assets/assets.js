9853ce3cd57b36dd1b2648bd9744d339fb2b4ff6
Tapas <tdalui865@gmail.com>
2024-07-08 23:02:16 +0530

    update explore list

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -113,19 +113,7 @@ export const menu_list = [
         menu_name: "Pasta",
         menu_image: menu_7
     },
-    {
-        menu_name: "Noodles",
-        menu_image: menu_8
-    },
-    {
-        menu_name: "Noodles",
-        menu_image: menu_8
-    },
-    {
-        menu_name: "Noodles",
-        menu_image: menu_8
-    }
-
+    
 ]
 
 
