ea9c6473760278deeee7da3411a7d0402958b395
Tapas <tdalui865@gmail.com>
2024-07-08 22:58:06 +0530

    change in navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,72 +1,84 @@
-import React, { useContext, useState } from 'react'
-import './Navbar.css'
-import { assets } from '../../assets/assets'
-import { Link } from 'react-scroll'
-import { NavLink } from 'react-router-dom'
-import { StoreContext } from '../../Context/StoreContext'
+import React, { useContext, useState } from "react";
+import "./Navbar.css";
+import { assets } from "../../assets/assets";
+import { Link } from "react-scroll";
+import { NavLink, useNavigate } from "react-router-dom";
+import { StoreContext } from "../../Context/StoreContext";
+import { FaShoppingCart } from "react-icons/fa";
+import { IoSearch } from "react-icons/io5";
 
-
-function Navbar({setShowLogin}) {
-
+function Navbar({ setShowLogin }) {
     // const [menu, setMenu] = useState("home")
-    const {getTotalCartAmount} = useContext(StoreContext)
+    const { getTotalCartAmount } = useContext(StoreContext);
 
+    const navigate = useNavigate()
     return (
         <>
             <div className="navbar">
-                
-                <img src={assets.logo} alt="" className="logo" />
-               
+                <img src={assets.logo} alt="" className="logo" onClick={()=> navigate('/')} />
+
                 <ul className="navbar-menu">
-                    <Link
-                        to="/" 
-                        spy={true} 
-                        smooth={true} 
-                        offset={-10} 
-                        duration={500} 
-                    >
-                        Home
-                    </Link>
-                    <Link
-                        to="explore-menu" 
-                        spy={true} 
-                        smooth={true} 
-                        offset={-10} 
-                        duration={500} 
-                    >
-                        Menu
-                    </Link>
-                    <Link
-                        to="app-download" 
-                        spy={true} 
-                        smooth={true} 
-                        offset={-10} 
-                        duration={500} 
-                    >
-                        Download App
-                    </Link>
-                    <Link
-                        to="footer" 
-                        spy={true} 
-                        smooth={true} 
-                        offset={-10} 
-                        duration={500} 
-                    >
-                        Contact
-                    </Link>
-                    
+                    <li>
+                        <Link
+                            to="header"
+                            spy={true}
+                            smooth={true}
+                            offset={-160}
+                            duration={500}
+                        >
+                            Home
+                        </Link>
+                    </li>
+
+                    <li>
+                        <Link
+                            to="explore-menu"
+                            spy={true}
+                            smooth={true}
+                            offset={-150}
+                            duration={500}
+                        >
+                            Menu
+                        </Link>
+                    </li>
+
+                    <li>
+                        <Link
+                            to="app-download"
+                            spy={true}
+                            smooth={true}
+                            offset={-200}
+                            duration={500}
+                        >
+                            Download App
+                        </Link>
+                    </li>
+
+                    <li>
+                        <Link
+                            to="footer"
+                            spy={true}
+                            smooth={true}
+                            offset={-10}
+                            duration={500}
+                        >
+                            Contact
+                        </Link>
+                    </li>
                 </ul>
                 <div className="navbar-right">
-                    <img src={assets.search_icon} alt="" />
-                    <div className="navbar-search-icon">
-                        <NavLink to='/cart'><img src={assets.basket_icon} alt="" /></NavLink>
-                        <div className={getTotalCartAmount()===0 ? " " : "dot" }></div>
+                    <IoSearch className="icons-sm"/>
+                    <div className="navbar-search-icon-sm">
+                        <NavLink to="/cart">
+                            <FaShoppingCart className="icons-sm"/>
+                        </NavLink>
+                        <div className="dot"></div>
                     </div>
-                    <button onClick={()=>setShowLogin(true)} >Sign in</button>
+                    <button onClick={() => setShowLogin(true)}>Sign in</button>
                 </div>
             </div>
         </>
-    )
+    );
 }
 
-export default Navbar
+export default Navbar;
